4b3f9fae2f01eb9d45037964c729b3af22f96703
David Wong <davidwg@calibra.com>
2021-01-25 10:44:23 -1000

    removing vuex

--- web-frontend/src/main.js
+++ web-frontend/src/main.js
@@ -1,21 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createStore } from 'vuex'
-
-// Create a new store instance.
-const store = createStore({
-  state() {
-    return {
-      count: 0
-    }
-  },
-  mutations: {
-    increment(state) {
-      state.count++
-    }
-  }
-})
 
 const app = createApp(App);
-app.use(store);
 app.mount('#app');
