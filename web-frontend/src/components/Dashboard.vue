c034a2d6f697f3d5f4dce36c09aae6fdbffc5b55
David Wong <davidwg@calibra.com>
2021-01-25 21:56:12 -1000

    [frontend] split dev & non-dev in two tables

--- web-frontend/src/components/Dashboard.vue
+++ web-frontend/src/components/Dashboard.vue
@@ -5,39 +5,17 @@
     <p># non-dev direct dependencies: {{ direct_dependencies }}</p>
     <p># non-dev transitive dependencies: {{ transitive_dependencies }}</p>
     <p># direct dev dependencies: {{ dev_dependencies }}</p>
-    <h2>updates available</h2>
-    <table class="table table-light table-striped table-hover">
-      <thead style="position: sticky; top: 0">
-        <tr>
-          <th class="header" scope="col">name</th>
-          <th class="header" scope="col">direct?</th>
-          <th class="header" scope="col">dev?</th>
-          <th class="header" scope="col">version change</th>
-          <th class="header" scope="col">rustsec</th>
-          <th class="header" scope="col">create PR (unless review needed)</th>
-          <th class="header" scope="col">changelog</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="d in updatable_dependencies" v-bind:key="d.name">
-          <td>{{ d.name }}</td>
-          <td>{{ d.direct }}</td>
-          <td>{{ d.dev }}</td>
-          <td>{{ d.version }} -> {{ d.new_version }}</td>
-          <td>{{ d.rustsec }}</td>
-          <td>
-            <a @click="copy_to_clipboard">click to create a PR</a>
-            <span class="invisible">{{ d.create_PR }}</span>
-          </td>
-          <td></td>
-        </tr>
-      </tbody>
-    </table>
+    <h2>Updates available for non-dev dependencies</h2>
+    <DependenciesTable v-bind:dependencies="non_dev_updatable_deps" />
+    <h2>Updates available for dev dependencies</h2>
+    <DependenciesTable v-bind:dependencies="dev_updatable_deps" />
   </section>
 </template>
 
 <script>
 import axios from "axios";
+
+import DependenciesTable from "./Dependencies.vue";
 
 export default {
   name: "Dashboard",
@@ -45,17 +23,27 @@ export default {
     return {
       commit: null,
       dependencies: null,
-      updatable_dependencies: null,
+      dev_updatable_deps: null,
+      non_dev_updatable_deps: null,
     };
   },
   mounted() {
     axios.get("/dependencies").then((response) => {
       this.commit = response.data.commit;
       this.dependencies = response.data.rust_dependencies.dependencies;
-      this.updatable_dependencies = response.data.rust_dependencies.dependencies.filter(
+      var updatable_dependencies = response.data.rust_dependencies.dependencies.filter(
         (dependency) => dependency.new_version != null
       );
+      this.non_dev_updatable_deps = updatable_dependencies.filter(
+        (dependency) => !dependency.dev
+      );
+      this.dev_updatable_deps = updatable_dependencies.filter(
+        (dependency) => dependency.dev
+      );
     });
+  },
+  components: {
+    DependenciesTable,
   },
   methods: {
     copy_to_clipboard() {
---
+++ web-frontend/src/components/Dependencies.vue
@@ -0,0 +1,48 @@
+<template>
+  <table class="table table-light table-striped table-hover">
+    <thead style="position: sticky; top: 0">
+      <tr>
+        <th class="header" scope="col">name</th>
+        <th class="header" scope="col">direct?</th>
+        <th class="header" scope="col">dev?</th>
+        <th class="header" scope="col">version change</th>
+        <th class="header" scope="col">rustsec</th>
+        <th class="header" scope="col">create PR (unless review needed)</th>
+        <th class="header" scope="col">changelog</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="d in dependencies" v-bind:key="d.name">
+        <td>{{ d.name }}</td>
+        <td>{{ d.direct }}</td>
+        <td>{{ d.dev }}</td>
+        <td>{{ d.version }} -> {{ d.new_version }}</td>
+        <td>{{ d.rustsec }}</td>
+        <td>
+          <a @click="copy_to_clipboard">click to create a PR</a>
+          <span class="invisible">{{ d.create_PR }}</span>
+        </td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "DependenciesTable",
+  props: {
+    dependencies: [],
+  },
+  setup(props) {
+    console.log(props.dependencies);
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  position: sticky;
+  top: 0;
+}
+</style>
