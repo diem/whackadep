2564757f4b300c6227dab0cec026876a89e0eae1
David Wong <davidwg@calibra.com>
2021-01-28 16:58:08 -1000

    revoked my personal access token that was accidently pushed to this repo :)

--- web-backend/dependabot/changelog.rb
+++ web-backend/dependabot/changelog.rb
@@ -1,4 +1,4 @@
-require "dependabot/omnibus"
+kdrequire "dependabot/omnibus"
 require 'dependabot/metadata_finders'
 
 credentials =
@@ -6,7 +6,7 @@ credentials =
     "type" => "git_source",
     "host" => "github.com",
     "username" => "x-access-token",
-    "password" => "6f8c56b758fdb7df88d2ce3acd30c1085f4e7b36"
+    "password" => ENV["GITHUB_TOKEN"]
   }]
 
 dependency = Dependabot::Dependency.new(
@@ -23,11 +23,11 @@ metadata_finder = Dependabot::MetadataFinders.for_package_manager("cargo").new(
   credentials: credentials
 )
 
-metadata = {
+update_metadata = {
   "changelog_url" => metadata_finder.changelog_url,
   "changelog_text" => metadata_finder.changelog_text,
   "commits_url" => metadata_finder.commits_url,
   "commits_text" => metadata_finder.commits_text,
 }
 
-puts metadata.to_json
+puts update_metadata.to_json
