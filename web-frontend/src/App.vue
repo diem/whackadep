5926c8f77908568603933a6280d24949d79b9119
David Wong <davidwg@calibra.com>
2021-02-10 11:24:59 -1000

    [frontend] mock up for multi-repo support

--- web-frontend/src/App.vue
+++ web-frontend/src/App.vue
@@ -17,20 +17,27 @@
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="/"
-              >Home <span class="sr-only">(current)</span></a
-            >
+            <a class="nav-link" href="/">
+              Home <span class="sr-only">(current)</span>
+            </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" @click.prevent="refresh" href="#">Refresh</a>
           </li>
         </ul>
-        <span class="navbar-text"
-          ><a href="https://github.com/diem/diem" target="_blank"
-            >diem/diem</a
-          ></span
-        >
       </div>
+
+      <b-dropdown id="dropdown-1" :text="current_repo" class="m-md-2">
+        <b-dropdown-item
+          v-for="repo in repos"
+          v-bind:key="repo"
+          :disabled="repo == current_repo"
+        >
+          {{ repo }}
+        </b-dropdown-item>
+        <b-dropdown-divider></b-dropdown-divider>
+        <b-dropdown-item>add a new rust repository</b-dropdown-item>
+      </b-dropdown>
     </nav>
 
     <Dashboard />
@@ -45,6 +52,15 @@ export default {
   name: "App",
   components: {
     Dashboard,
+  },
+  data() {
+    return {
+      current_repo: "https://github.com/diem/diem.git",
+      repos: [
+        "https://github.com/diem/diem.git",
+        "https://github.com/diem/operations.git",
+      ],
+    };
   },
   methods: {
     refresh() {
