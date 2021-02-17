db7371e06c6b3593c7f0f1a73cdb4cfb9053dffd
David Wong <davidwg@calibra.com>
2021-02-16 17:19:00 -1000

    [frontend] moving stuff to utils/

--- web-frontend/src/components/Dashboard.vue
+++ web-frontend/src/components/Dashboard.vue
@@ -138,6 +138,7 @@ import axios from "axios";
 import DependenciesTable from "./Dependencies.vue";
 import RustsecTable from "./Rustsec.vue";
 
+import { update_allowed } from "@/utils/dependencies";
 import { calculate_priority_score } from "@/engines/priority";
 import { calculate_risk_score } from "@/engines/risk";
 
@@ -279,7 +280,7 @@ export default {
             // only modify dependencies that have update now
             if (dependency.update != null) {
               // can we update this?
-              if (dependency.direct || this.update_allowed(dependency)) {
+              if (dependency.direct || update_allowed(dependency)) {
                 dependency.update_allowed = true;
               } else {
                 dependency.update_allowed = false;
@@ -388,41 +389,6 @@ export default {
         variant: variant,
         solid: true,
       });
-    },
-    // This checks if a dependency can be updated in several senses:
-    // - if it's a direct dependency, can it be updated easily (no breaking changes, if the developers respected Rust variant of semver)
-    // - if it's a transitive dependency, can we update it at all?
-    // https://doc.rust-lang.org/cargo/reference/specifying-dependencies.html#caret-requirements
-    // > An update is allowed if the new version number does not modify the left-most non-zero digit in the major, minor, patch grouping
-    // > This compatibility convention is different from SemVer in the way it treats versions before 1.0.0. While SemVer says there is no compatibility before 1.0.0, Cargo considers 0.x.y to be compatible with 0.x.z, where y ≥ z and x > 0.
-    update_allowed(dependency) {
-      var version = dependency.version;
-      var new_version =
-        dependency.update.versions[dependency.update.versions.length - 1];
-
-      var pre = this.predicate(version);
-      return semver.satisfies(new_version, pre);
-    },
-    predicate(version) {
-      var major = semver.major(version);
-      if (major != 0) {
-        return `${major}.x`;
-      }
-      var minor = semver.minor(version);
-      if (minor != 0) {
-        return `${major}.${minor}.x`;
-      }
-      var patch = semver.patch(version);
-      if (patch != 0) {
-        return `${major}.${minor}.${patch}.x`;
-      }
-      var prerelease = semver.prerelease(version);
-      if (prerelease != 0) {
-        return `${major}.${minor}.${patch}.${prerelease}.x`;
-      }
-      // if we can't figure it out, avoid false negative by
-      // return a predicate that will say "yes we can update this"
-      return "x";
     },
   },
   computed: {
