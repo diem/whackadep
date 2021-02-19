b94791d854c4fe2d06553c2eedbb49660473b638
John Naulty <jnaulty@fb.com>
2021-02-18 16:43:34 -0800

    fix scripts; update Makefile for kustomize

--- scripts/create-docker-images.sh
+++ scripts/create-docker-images.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+repo="localhost:5000"
 build_dirs=( "web-backend" "web-frontend")
 for build in ${build_dirs[@]}; do 
   pushd ${build}
-  docker build -t ${build} .
+  docker build -t "${repo}/${build}" .
   popd
 done;
