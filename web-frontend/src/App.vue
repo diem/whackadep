f3156acd00b80242185109bf248001eccab2abd0
Nasif Imtiaz <nasifimtiaz@fb.com>
2021-06-08 10:03:41 -0400

    fetch health metrics in the frontend and display in a new page

--- web-frontend/src/App.vue
+++ web-frontend/src/App.vue
@@ -129,10 +129,14 @@ export default {
     },
 
     // attempts to start an analysis on a given repo
+    // TODO: 1. currently we (re-)start analysis on referesh click which is not the best UI,
+    //       2. we can have have separate button to navigate us towards separate analysis pages
+    //       3. when a new repository is selected, the page must be automatically refreshed to erase past values
     refresh() {
       axios.get("/refresh?repo=" + this.current_repo).then((response) => {
         if (response.data == "ok") {
           this.toast(
+            //QUESTION: How is this order maintained?
             "Refresh requested",
             "analysis started, please refresh the page in a bit...",
             "success"
@@ -269,6 +273,18 @@ export default {
           text: this.current_repo,
           to: { name: "repo", params: { repo: this.current_repo } },
         });
+      }
+
+      // TODO: Need to add a button which will activate this route change
+      else if (to.name == 'dependency_health') {
+        this.current_repo = to.params.repo;
+        this.change_route(
+          {
+            text: this.current_repo,
+            to: { name: "repo", params: { repo: this.current_repo } },
+          }
+        );
+
       }
 
       // review
