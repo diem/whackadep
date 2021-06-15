fd4f433ca16f67457fb827d8e576a8203006b8d2
Nasif Imtiaz <nasifimtiaz@fb.com>
2021-06-14 22:49:20 -0400

    fetching GitHub metrics in analysis

--- analysis/src/lib.rs
+++ analysis/src/lib.rs
@@ -5,6 +5,7 @@ use guppy::graph::PackageMetadata;
 use tabled::Tabled;
 
 mod cratesio;
+mod github;
 
 #[derive(Tabled)]
 pub struct DependencyReport<'a> {
@@ -13,6 +14,10 @@ pub struct DependencyReport<'a> {
     pub hosted_on_cratesio: bool,
     pub cratesio_downloads: u64,
     pub cratesio_dependents: u64,
+    pub hosted_on_github: bool,
+    pub github_stars: u64,
+    pub github_subscribers: u64,
+    pub github_forks: u64,
 }
 
 pub struct DependencyAnalyzer;
@@ -26,12 +31,20 @@ impl DependencyAnalyzer {
         let cratesio_report = cratesio::CratesioAnalyzer::new()?;
         let cratesio_report = cratesio_report.analyze_cratesio(package)?;
 
+        // GitHub API analysis
+        let github_report = github::GitHubAnalyzer::new()?;
+        let github_report = github_report.analyze_github(package)?;
+
         let dependency_report = DependencyReport {
             name,
             has_build_script,
             hosted_on_cratesio: cratesio_report.is_hosted,
             cratesio_downloads: cratesio_report.downloads,
             cratesio_dependents: cratesio_report.dependents,
+            hosted_on_github: github_report.is_github_repo,
+            github_stars: github_report.repo_stats.stargazers_count,
+            github_subscribers: github_report.repo_stats.subscribers_count,
+            github_forks: github_report.repo_stats.forks,
         };
 
         Ok(dependency_report)
